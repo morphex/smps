8c565c561c0a9593f4d5e014ceb1b59afc395567
Morten W. Petersen <morphex@gmail.com>
2022-07-18 17:38:30 +0200

    Adding thread-safe message queue and updating test.

--- client.py
+++ client.py
@@ -15,6 +15,18 @@ time.sleep(5)
 
 print("From server:", ssl_connection.recv(1024).decode())
 
+time.sleep(2)
+
+ssl_connection.send("list".encode())
+
+time.sleep(5)
+
+messages = ssl_connection.recv(1024).decode()
+print("From server:", messages)
+
+messages = ssl_connection.recv(1024).decode()
+print("From server:", messages)
+
 ssl_connection.send("QUIT".encode())
 
 time.sleep(5)
--- smps.py
+++ smps.py
@@ -6,18 +6,64 @@ import ssl
 import socket
 import time
 import sys
+import threading
+
+class SimpleMessageStorage:
+
+    def __init__(self, expiry=600):
+        """expiry is seconds a message is valid."""
+        self.messages = []
+        self.expiry = expiry
+        self._lock = threading.Lock()
+
+    def add_message(self, message):
+        self._acquire_lock()
+        self.messages.append((time.time(), message))
+        self._release_lock()
+
+    def _acquire_lock(self):
+        """Acquires a lock on the message database."""
+        print("Locking message database")
+        self._lock.acquire()
+
+    def _release_lock(self):
+        """Acquires a lock on the message database."""
+        print("Releasing message database")
+        self._lock.release()
+
+    def get_messages(self):
+        """Returns a list of messages that are still valid, and
+           clears out old messages."""
+        new_messages = []
+        self._acquire_lock()
+        for timestamp, message in self.messages:
+            if timestamp < (time.time() + self.expiry):
+                new_messages.append((timestamp, message))
+        self.messages = new_messages
+        self._release_lock()
+        return self.messages
+
+message_storage = SimpleMessageStorage()
 
 class Handler(StreamRequestHandler):
 
     def handle(self):
+        global message_storage
         try:
             while True:
                 input = self.request.recv(1024)
                 input = input.decode("utf-8")
                 print("From client:", input)
+                message_storage.add_message(input)
                 self.wfile.write("Message accepted".encode())
                 time.sleep(2)
-                if input.lower() == "quit":
+                if input.lower().strip() == "list":
+                   print("Listing messages")
+                   messages = ""
+                   for timestamp, message in message_storage.get_messages():
+                       messages += "%f,%s\n" % (timestamp, message)
+                   self.wfile.write(messages.encode())
+                if input.lower().strip() == "quit":
                    print("Exiting")
                    self.server.shutdown()
         except Exception as instance:
